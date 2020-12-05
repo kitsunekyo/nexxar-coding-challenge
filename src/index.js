d06ed61f926e012cecca44a6aa5004754ff8c38c
Alex Spieslechner <alexander.spieslechner@gmail.com>
2020-12-05 17:31:26 +0100

    man i cant member

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 (function () {
     document.addEventListener("DOMContentLoaded", () => {
+        drawer();
         menu();
     });
 
-    const menu = () => {
+    const drawer = () => {
         let isOpen = false;
-        let backupHeaderText = '';
+        let backupHeaderText = "";
 
-        const $header = document.querySelector("header");
+        const $body = document.querySelector("body");
         const $headerTitleEl = document.querySelector(".header__title");
 
         const toggle = () => {
@@ -15,10 +16,42 @@
                 backupHeaderText = $headerTitleEl.innerHTML;
             }
             isOpen = !isOpen;
-            $header.classList.toggle("header--open");
-            $headerTitleEl.innerHTML = isOpen ? 'Menu' : backupHeaderText;
-        }
-
+            $body.classList.toggle("drawer--open");
+            $headerTitleEl.innerHTML = isOpen ? "Menu" : backupHeaderText;
+        };
+        document.querySelector('.overlay').addEventListener('click', toggle);
         document.querySelector(".nav-button").addEventListener("click", toggle);
-    }
+    };
+
+    const menu = () => {
+        let activeMenu = "main";
+        const $mainMenuEl = document.querySelector(".menu");
+        const $secondaryMenuEl = document.querySelector(".menu--secondary");
+
+        const handleNavItemClick = (e) => {
+            e.preventDefault();
+            navigate(e.target.dataset.menuTarget);
+        };
+
+        const navigate = (target) => {
+            activeMenu = target;
+
+            switch (activeMenu) {
+                case "main":
+                    $mainMenuEl.style.display = "block";
+                    $secondaryMenuEl.style.display = "none";
+                    break;
+                case "secondary":
+                    $mainMenuEl.style.display = "none";
+                    $secondaryMenuEl.style.display = "block";
+                    break;
+                default:
+                    break;
+            }
+        };
+
+        document.querySelectorAll("[data-menu-target]").forEach((el) => {
+            el.addEventListener("click", handleNavItemClick);
+        });
+    };
 })();
