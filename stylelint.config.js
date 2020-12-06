4d7000a9c1d84c56acce1914924b746ef07108a1
Alex Spieslechner <alexander.spieslechner@gmail.com>
2020-12-06 15:08:17 +0100

    fix: remove scss stylelint configs

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,8 @@
 /* eslint-env node */
 module.exports = {
     extends: ["stylelint-config-standard", "stylelint-config-prettier"],
-    plugins: ["stylelint-scss"],
     rules: {
         "declaration-no-important": true,
         "at-rule-no-unknown": null,
-        "scss/at-rule-no-unknown": true,
     },
 };
